3f57595279b5b4b3517f420dabe2d6d511a5b564
Marcel Mravec <marcel.mravec@gmail.com>
2024-12-03 08:49:42 +0100

    day 03 part 1 done

--- AoC2024/day03.swift
+++ AoC2024/day03.swift
@@ -6,3 +6,39 @@
 //
 
 import Foundation
+
+enum Day03 {
+    static func run() {
+        let input1 = readFile("day03.input")
+        
+        let result = day03Part1(input1)
+        print(result)
+//        print(day03Part2(input1))
+    }
+}
+
+func day03Part1(_ input: String) -> Int {
+    let pattern = #"mul\((\d{1,3}),(\d{1,3})\)"#
+
+    var sum = 0
+
+    do {
+        let regex = try NSRegularExpression(pattern: pattern)
+        let matches = regex.matches(in: input, range: NSRange(input.startIndex..., in: input))
+        
+        for match in matches {
+            if let firstNumberRange = Range(match.range(at: 1), in: input),
+               let secondNumberRange = Range(match.range(at: 2), in: input) {
+                let firstNumber = Int(input[firstNumberRange]) ?? 0
+                let secondNumber = Int(input[secondNumberRange]) ?? 0
+                sum += firstNumber * secondNumber
+            }
+        }
+        
+        print("Sum of products: \(sum)")
+        return sum
+    } catch {
+        print("Invalid regex: \(error.localizedDescription)")
+    }
+    return 0
+}
