3ccd6b6c8af0fcb3ca31591e4914afc70c919e2e
Marcel Mravec <marcel.mravec@gmail.com>
2024-12-04 09:44:59 +0100

    day04 part 1 finished

---
+++ AoC2024/day04.swift
@@ -0,0 +1,68 @@
+//
+//  day04.swift
+//  AoC2024
+//
+//  Created by Marcel Mravec on 04.12.2024.
+//
+
+import Foundation
+
+enum Day04 {
+    static func run() {
+        var input1: String = ""
+        do {
+            input1 = try readFile("day04.input")
+        } catch {
+            print("Error reading input file.")
+        }
+        let result = day04Part1(input1)
+        print(result)
+        //        print(day04Part2(input1))
+    }
+}
+
+func day04Part1(_ input: String) -> Int {
+    let directions = [
+        (-1, -1), (-1, 0), (-1, 1),  // Top-left, top, top-right
+        ( 0, -1),         ( 0, 1),  // Left, right
+        ( 1, -1), ( 1, 0), ( 1, 1)  // Bottom-left, bottom, bottom-right
+    ]
+    let search = Array("MAS")
+    var numberOfXmas = 0
+    var matrix = [[Character]]()
+    
+    let lines = input.split(separator: "\n").map { $0.trimmingCharacters(in: .whitespacesAndNewlines)}
+    lines.forEach { line in
+        matrix.append(Array(line))
+    }
+    for row in 0..<matrix.count {
+        for col in 0..<matrix[row].count {
+            if matrix[row][col] == "X" {
+                for direction in directions {
+                    var match = true
+                    for i in 0..<search.count {
+                        let newRow = row + direction.0 * (i + 1)
+                        let newCol = col + direction.1 * (i + 1)
+                        
+                        // Check if the new position is out of bounds
+                        if newRow < 0 || newRow >= matrix.count || newCol < 0 || newCol >= matrix[0].count {
+                            match = false
+                            break
+                        }
+                        
+                        // Check if the character matches
+                        if matrix[newRow][newCol] != search[i] {
+                            match = false
+                            break
+                        }
+                    }
+                    if match {
+                        numberOfXmas += 1
+                    }
+                    
+                }
+            }
+        }
+    }
+    return numberOfXmas
+}
--- AoC2024/main.swift
+++ AoC2024/main.swift
@@ -9,8 +9,8 @@ import Foundation
 
 //Day01.run()
 //Day02.run()
-Day03.run()
-//Day04.run()
+//Day03.run()
+Day04.run()
 //Day05.run()
 //Day06.run()
 //Day07.run()
