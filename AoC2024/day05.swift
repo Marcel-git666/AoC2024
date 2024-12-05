787de744e043abf579743c6c5ea70ea117e59318
Marcel Mravec <marcel.mravec@gmail.com>
2024-12-05 11:12:12 +0100

    day 05 part 1

---
+++ AoC2024/day05.swift
@@ -0,0 +1,87 @@
+//
+//  day05.swift
+//  AoC2024
+//
+//  Created by Marcel Mravec on 05.12.2024.
+//
+
+import Foundation
+
+enum Day05 {
+    static func run() {
+        var input: String = ""
+        do {
+            input = try readFile("day05.input")
+        } catch {
+            print("Error reading input file.")
+        }
+        let parts = input.split(separator: "\n\n").map { $0.trimmingCharacters(in: .whitespacesAndNewlines)}
+        let result = day05Part1(getDictionary(parts[0]), updates: getUpdates(parts[1]))
+        print(result)
+        print(day05Part2(getDictionary(parts[0]), updates: getUpdates(parts[1])))
+    }
+}
+
+func getDictionary(_ input: String) -> [Int: Set<Int>] {
+    var dict: [Int: Set<Int>] = [:]
+    for ordering in input.split(separator: "\n") {
+        let line = ordering.split(separator: "|").compactMap { Int($0) }
+        if dict[line[0]] != nil {
+            dict[line[0]]!.formUnion(line[1...])
+        } else {
+            dict[line[0]] = Set(line[1...])
+        }
+    }
+    return dict
+}
+
+func getUpdates(_ input: String) -> [[Int]] {
+    input.split(separator: "\n").map { line in
+        line.trimmingCharacters(in: .whitespacesAndNewlines).split(separator: ",").compactMap { Int($0) }
+    }
+}
+
+func day05Part1(_ dict: [Int: Set<Int>], updates: [[Int]]) -> Int {
+    var sum = 0
+    var isOk = true
+    updates.forEach { update in
+        for i in 0..<update.count - 1 {
+            if dict[update[i]]?.contains(update[i+1]) ?? false {
+                isOk = true
+            }
+            else {
+                isOk = false
+                break
+            }
+        }
+        if isOk {
+            print(update[update.count/2])
+            sum += update[update.count/2]
+        }
+    }
+    
+    return sum
+}
+
+func day05Part2(_ dict: [Int: Set<Int>], updates: [[Int]]) -> Int {
+    var sum = 0
+    var isOk = true
+    updates.forEach { update in
+        for i in 0..<update.count - 1 {
+            if dict[update[i]]?.contains(update[i+1]) ?? false {
+                isOk = true
+            }
+            else {
+                isOk = false
+                break
+            }
+        }
+        if isOk {
+            print(update[update.count/2])
+            sum += update[update.count/2]
+        }
+    }
+    
+    return sum
+}
+
